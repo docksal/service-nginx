ddcfc229ff4b61f3f67443ad4223a0c328da658f
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-02-08 11:27:46 -0800

    Renamed entrypoint scripts
    - Renamed entrypoint scripts to follow the naming convention in other repos
    - Updated indentation to use tabs

--- docker-entrypoint.d/01-config-templates.sh
+++ docker-entrypoint.d/01-config-templates.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+set -e # Fail on errors
+
 process_template()
 {
-    src=${1}
-    dst=${2}
-    cp -f ${src} ${dst}
+	src=${1}
+	dst=${2}
+	cp -f ${src} ${dst}
 
-    for variable in NGINX_VHOST_PRESET NGINX_BACKEND_HOST NGINX_BACKEND_PORT NGINX_VHOST_PRESET NGINX_SERVER_ROOT
-    do
+	for variable in NGINX_VHOST_PRESET NGINX_BACKEND_HOST NGINX_BACKEND_PORT NGINX_VHOST_PRESET NGINX_SERVER_ROOT
+	do
 	eval value=\$${variable}
 	sed -i 's#{{ '${variable}' }}#'${value}'#g' ${dst}
-    done
+	done
 }
 
 :>/etc/nginx/includes/upstream.conf
@@ -23,10 +25,9 @@ process_template "/etc/nginx/includes/defaults.conf.tmpl" "/etc/nginx/includes/d
 
 if [[ -n "${NGINX_VHOST_PRESET}" ]]
 then
-    process_template "/etc/nginx/presets/${NGINX_VHOST_PRESET}.conf.tmpl" "/etc/nginx/includes/preset.conf"
-    if [[ "${NGINX_VHOST_PRESET}" =~ ^drupal|wordpress|php$ ]]
-    then
-        process_template "/etc/nginx/includes/upstream.php.conf.tmpl" "/etc/nginx/includes/upstream.conf"
-    fi
+	process_template "/etc/nginx/presets/${NGINX_VHOST_PRESET}.conf.tmpl" "/etc/nginx/includes/preset.conf"
+	if [[ "${NGINX_VHOST_PRESET}" =~ ^drupal|wordpress|php$ ]]
+	then
+		process_template "/etc/nginx/includes/upstream.php.conf.tmpl" "/etc/nginx/includes/upstream.conf"
+	fi
 fi
-
--- docker-entrypoint.d/02-user-overrides.sh
+++ docker-entrypoint.d/02-user-overrides.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-set -e
+
+set -e # Fail on errors
 
 # Support for configuration overrides
 include_conf ()
 {
-    src=${1}
-    dst=${2}
-    if [[ -f "${src}" ]]
-    then
+	src=${1}
+	dst=${2}
+	if [[ -f "${src}" ]]
+	then
 	echo "Including configuration overrides from ${src}"
 	cp "${src}" "${dst}"
-    fi
+	fi
 }
 
 include_conf "/var/www/.docksal/etc/nginx/vhost-overrides.conf" "/etc/nginx/includes/defaults.conf"
 include_conf "/var/www/.docksal/etc/nginx/vhosts.conf" "/etc/nginx/conf.d/user-vhost.conf"
-
